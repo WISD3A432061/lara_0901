9f9e05bacd5633232a82f7305117a20998b2baab
陳姿穎 <roeyang1410@gmail.com>
2018-01-02 23:46:18 +0800

    修正練習6-1、6-5

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use App\Http\PostRequests;
 use App\Post;
 class AdminPostsController extends Controller
 {
@@ -22,14 +22,22 @@ class AdminPostsController extends Controller
 
     public function edit($id)
     {
-        $data = ['id' => $id];
-
+        $post = Post::find($id);
+        $data = ['post' => $post];
         return view('admin.posts.edit', $data);
     }
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
     }
+
+    public function update(Request $request, $id)
+     {
+         $post = Post::find($id);
+         $post->update($request->all());
+         return redirect()->route('admin.posts.index');
+     }
+
 
 }
