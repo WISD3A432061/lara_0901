7ce407f2fadf43809a3beffa5702915b37190957
陳姿穎 <roeyang1410@gmail.com>
2018-01-02 23:48:43 +0800

    復原PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,33 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
 class PostsController extends Controller
 {
     public function index()
     {
         return view('posts.index');
     }
-
-    public function edit($id)
+    public function show($id)
     {
-        $post = Post::find($id);
-        $data = ['post' => $post];
-        return view('admin.posts.edit', $data);
+        $data = ['id' => $id];
+        return view('posts.show', $data);
     }
-
-    public function update(Request $request, $id)
-    {
-        $post = Post::find($id);
-        $post->update($request->all());
-        return redirect()->route('admin.posts.index');
-    }
-
-    public function destroy($id)
-    {
-        Post::destroy($id);
-        return redirect()->route('admin.posts.index');
-    }
-
-
 }
