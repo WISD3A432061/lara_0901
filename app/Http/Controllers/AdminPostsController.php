1d651901623a65b8bee4d48719a9cec84dd66380
陳姿穎 <roeyang1410@gmail.com>
2018-01-02 22:29:14 +0800

    鍊習5-5：設定頁面跳轉

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -29,6 +29,7 @@ class AdminPostsController extends Controller
     public function store(Request $request)
     {
         Post::create($request->all());
+        return redirect()->route('admin.posts.index');
     }
 
 }
