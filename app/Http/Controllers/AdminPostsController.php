b5f8442b2b86b89d34df3f2d98f980a9e4ab9b9e
陳姿穎 <roeyang1410@gmail.com>
2018-01-02 23:50:28 +0800

    修正練習7-3

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -39,5 +39,10 @@ class AdminPostsController extends Controller
          return redirect()->route('admin.posts.index');
      }
 
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
 
 }
